92fd5f89f4462df4cad6a9a56b2305d3bfa7a97a
root <Gokkulnath>
2021-01-21 12:57:50 +0000

    setup_script

--- setup_env.sh
+++ setup_env.sh
@@ -1,1 +1,8 @@
-pip install -r requirements.txt
+
+git clone https://github.com/POSTECH-CVLab/PyTorch-StudioGAN.git
+git clone https://github.com/OmidPoursaeed/Generative_Adversarial_Perturbations.git
+
+git config --global user.email gokkulnath@gmail.com
+git config --global user.email Gokkulnath
+
+#pip install -r requirements.txt
